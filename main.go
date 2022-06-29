793a906800b6855ddb8e513708c0edfbde039d1d
AKGL1546 <105883710+AKGL1546@users.noreply.github.com>
2022-06-30 02:25:38 +0800

    milulu

--- main.go
+++ main.go
@@ -204,8 +204,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 2328585538)
+	sus = append(sus, 2822449052)
 
 	// 启用 gui
 	// webctrl.InitGui(*g)
